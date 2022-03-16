70b8503537a9620d094283e55374dcef7ce55413
Nzovia <nickiemaundu19@gmail.com>
2022-03-16 22:32:42 +0300

    initializing project

---
+++ src/main/java/com/example/masomo/MasomoApplication.java
@@ -0,0 +1,13 @@
+package com.example.masomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasomoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/masomo/MasomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
